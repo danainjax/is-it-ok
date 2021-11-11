33d1f0400edd6017075099c58f74aa46fb0b9736
danainjax <danainjax@gmail.com>
2021-11-11 08:39:27 -0500

    fixed cproblem in c[B
    "

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import './App.css'
 
 const getUsers = () => {
-  fetch('http://localhost:4000/users')
+  fetch('http://localhost:3000/users')
     .then((response) => response.json())
     .then((data) => console.log(data[0].name))
 }
 getUsers()
 function App() {
   return (
-    <div className='App'>
-      <div
-        style={{
-          backgroundColor: 'green',
-          color: 'white',
-          fontFamily: 'Indie Flower',
-          textAlign: 'left',
-        }}
-      >
-        <h1>is-it-ok </h1>
-      </div>
+    <div
+      style={{
+        backgroundColor: 'green',
+        color: 'white',
+        fontFamily: 'Indie Flower',
+        textAlign: 'left',
+      }}
+    >
+      <h1>is-it-ok </h1>
     </div>
   )
 }
