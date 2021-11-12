78574c6b07f5b37bbc4fb1ae58808b1469a9e65b
danainjax <danainjax@gmail.com>
2021-11-12 13:08:45 -0500

    fetching data on user and then setting state with it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import './App.css'
+import React, { Component } from 'react'
+import User from './components/User'
 
-const getUsers = () => {
-  fetch('http://localhost:3000/users')
-    .then((response) => response.json())
-    .then((data) => console.log(data[0].name))
-}
-getUsers()
-function App() {
-  return (
-    <div
-      style={{
-        backgroundColor: 'green',
-        color: 'white',
-        fontFamily: 'Indie Flower',
-        textAlign: 'left',
-      }}
-    >
-      <h1>is-it-ok </h1>
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: 'Phineas',
+    }
+  }
+  componentDidMount() {
+    fetch('http://localhost:3000/users')
+      .then((response) => response.json())
+      .then((data) => this.setState({ name: data[0].name }))
+  }
+  render() {
+    return (
+      <div
+        style={{
+          backgroundColor: 'green',
+          color: 'white',
+          fontFamily: 'Indie Flower',
+          textAlign: 'left',
+        }}
+      >
+        <h1>is-it-ok </h1>
+        User: <User name={this.state.name} />
+      </div>
+    )
+  }
 }
 
 export default App
