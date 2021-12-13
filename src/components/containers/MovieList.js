b3e8f9d7a4b7f6168a2659f7fe6bddd788329843
danainjax <danainjax@gmail.com>
2021-12-13 13:53:32 -0500

    removed class from Movie List, made it a functional component

--- src/components/containers/MovieList.js
+++ src/components/containers/MovieList.js
@@ -1,23 +1,16 @@
-import React, { Component } from 'react'
-const apiKey = process.env.REACT_APP_IMDB_API_KEY
-class MovieList extends Component {
-  constructor(props) {
-    super()
-  }
-
-  render() {
-    return (
-      <div
-        style={{
-          backgroundColor: '#3a539b',
-          color: 'white',
-          fontFamily: 'Raleway',
-          textAlign: 'left',
-        }}
-      >
-        <h1>Movie List</h1>
-      </div>
-    )
-  }
+// const apiKey = process.env.REACT_APP_IMDB_API_KEY
+function MovieList(props) {
+  return (
+    <div
+      style={{
+        backgroundColor: '#3a539b',
+        color: 'white',
+        fontFamily: 'Raleway',
+        textAlign: 'left',
+      }}
+    >
+      <h1>Movie List</h1>
+    </div>
+  )
 }
 export default MovieList
