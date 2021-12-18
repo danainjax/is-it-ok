00cead5bd4bbda0b7d532a660a26330605e72548
danainjax <danainjax@gmail.com>
2021-12-18 09:00:21 -0500

    added redux devtools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import App from './App'
 import { Provider } from 'react-redux'
 import MovieReducer from './reducers/MovieReducer'
@@ -9,7 +9,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 const allReducers = combineReducers({ movieStore: MovieReducer })
 
-const store = createStore(allReducers, applyMiddleware(thunk))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(allReducers, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
