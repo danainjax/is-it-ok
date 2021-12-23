3aee8dc17bfd10927a3c9f49637e7e0dbaf06254
danainjax <danainjax@gmail.com>
2021-12-23 07:53:42 -0500

    added more options to navbar for build

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,16 @@ export const Navbar = () => {
           <NavLink to='/movies'>movies</NavLink>
         </li>
         <li>
-          <NavLink to='/'>home</NavLink>
+          <NavLink to='/movies'>now playing</NavLink>
+        </li>
+        <li>
+          <NavLink to='/user/:id'>my account</NavLink>
+        </li>
+        <li>
+          <NavLink to='/reviews'>reviews</NavLink>
+        </li>
+        <li>
+          <NavLink to='/search'>search</NavLink>
         </li>
       </ul>
     </div>
