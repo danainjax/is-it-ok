e1167628521aa3d9f4b86f3819245c51af2a4c06
danainjax <danainjax@gmail.com>
2022-01-06 19:12:08 -0500

    coded onChange in form fields

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,29 +1,61 @@
 import { useState } from 'react'
 
 function ReviewForm() {
+  const [rating, setRating] = useState(5)
+  const [sex, setSex] = useState('')
+  const [language, setLanguage] = useState('')
+  const [violence, setViolence] = useState('')
+  const [comment, setComment] = useState('')
   return (
     <>
       <form>
         <h1>Review form</h1>
         <label>
           Rating:
-          <input type='number' name='rating' min='1' max='5' />
+          <input
+            type='number'
+            name='rating'
+            min='1'
+            max='5'
+            onChange={(e) => setRating(e.target.value)}
+            value={rating}
+          />
         </label>
         <label>
           Sex:
-          <input type='textarea' name='sex' />
+          <input
+            type='textarea'
+            name='sex'
+            onChange={(e) => setSex(e.target.value)}
+            value={sex}
+          />
         </label>
         <label>
           Language:
-          <input type='textarea' name='language' />
+          <input
+            type='textarea'
+            name='language'
+            onChange={(e) => setLanguage(e.target.value)}
+            value={language}
+          />
         </label>
         <label>
           Violence:
-          <input type='textarea' name='violence' />
+          <input
+            type='textarea'
+            name='violence'
+            onChange={(e) => setViolence(e.target.value)}
+            value={violence}
+          />
         </label>
         <label>
           Comment:
-          <input type='textarea' name='comment' />
+          <input
+            type='textarea'
+            name='comment'
+            onChange={(e) => setComment(e.target.value)}
+            value={comment}
+          />
         </label>
         <input type='submit' value='Submit' />
       </form>
