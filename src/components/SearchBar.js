f0eda4d323e3ba8fd8cf2a9cb105a559c85f7128
danainjax <danainjax@gmail.com>
2022-02-21 10:30:26 -0500

    capturing search text

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,24 @@
 import { connect } from 'react-redux'
-// import { useState } from 'react'
+import { useState } from 'react'
 import '../style.css'
 
 function SearchBar() {
-  // const { title, setTitle } = useState('')
-  console.log('cheese')
+  const [searchText, setSearchText] = useState('')
+  const searchForMovie = (searchText) => {
+    setSearchText(searchText)
+    console.log(searchText)
+    //now we have the search text, need to make an action that filters the movies by the search text and returns the correct movie, or those closest
+  }
+
   return (
     <>
-      <form onSubmit={() => console.log('cheese')}>
+      <form onSubmit={(e) => searchForMovie(searchText)}>
         <input
           className='searchbar'
           type='text'
           name='movie'
           placeholder='search movie by title'
-          onChange={(event) => console.log(event.target.value)}
+          onChange={(e) => setSearchText(e.target.value)}
         />
         <button className='searchbar' type='submit'>
           submit
@@ -25,6 +30,7 @@ function SearchBar() {
 }
 const mapStateToProps = (state) => ({
   movie: state.movieStore.movie,
+  movies: state.movieStore.movies,
 })
 
 export default connect(mapStateToProps)(SearchBar)
